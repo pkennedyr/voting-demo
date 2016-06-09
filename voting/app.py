59a713cf7589e24b435fdbd560e8ceb12721a23c
Ryan Kennedy <ryankennedy@C02NH0DFG3QDs-MacBook-Pro.local>
2016-06-09 08:46:44 -0700

    Changed to hockey

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Warriors"
-optionB = "Cavs"
+optionA = "Sharks"
+optionB = "Penguins"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
